4835020a9385f4798efff833a53c3ad75bf8197d
Tristan Ross <tross@ezoic.com>
2022-10-07 11:32:58 -0700

    feat: added basic stuff

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "SDK for ExpidusOS";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "aarch64-linux"
+        "i686-linux"
+        "riscv64-linux"
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+      packagesFor = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in with pkgs; {
+          nativeBuildInputs = [ meson ninja pkg-config ];
+          buildInputs = [ uncrustify clang_14 ];
+        });
+    in rec {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          systemPackages = packagesFor.${system};
+        in {
+          default = pkgs.stdenv.mkDerivation rec {
+            name = "expidus-sdk";
+            src = self;
+
+            enableParallelBuilding = true;
+            inherit (systemPackages) nativeBuildInputs buildInputs;
+          };
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          systemPackages = packagesFor.${system};
+        in {
+          default = pkgs.mkShell {
+            packages = systemPackages.nativeBuildInputs ++ systemPackages.buildInputs;
+          };
+
+          wrapped = pkgs.mkShell {
+            packages = packages.${system}.default;
+          };
+        });
+    };
+}
