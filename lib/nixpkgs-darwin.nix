afea0fb23a49550e7d72e1b15ff84a217342b0e9
Tristan Ross <tross@ezoic.com>
2022-10-19 09:08:12 -0700

    chore: update nixpkgs

--- lib/nixpkgs-darwin.nix
+++ lib/nixpkgs-darwin.nix
@@ -1,4 +1,4 @@
 fetchTarball {
   url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixpkgs-22.05-darwin.tar.gz";
-  sha256 = "16vw0667j71vbnyayzpfwdxjkdmal531kv1mhn857a8hmhp2d0ns";
+  sha256 = "08bc7kp293zcv3sakyhj28z452l0734zbcxhwkh4fyd3i03rizz6";
 }
--- lib/nixpkgs.nix
+++ lib/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
   url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-22.05.tar.gz";
-  sha256 = "16vw0667j71vbnyayzpfwdxjkdmal531kv1mhn857a8hmhp2d0ns";
+  sha256 = "08bc7kp293zcv3sakyhj28z452l0734zbcxhwkh4fyd3i03rizz6";
 }
