a1ea5932bd60965a61ef7e173c653318f8cdc015
Tristan Ross <tristan.ross@midstall.com>
2022-10-20 08:37:29 -0700

    chore: update nixpkgs

--- lib/nixpkgs.nix
+++ lib/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
   url = "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-22.05.tar.gz";
-  sha256 = "08bc7kp293zcv3sakyhj28z452l0734zbcxhwkh4fyd3i03rizz6";
+  sha256 = "020bgh1nac5yi2qqaq8avcyhya78w4hjaz9a5g6rwkmwiqhj9cbv";
 }
