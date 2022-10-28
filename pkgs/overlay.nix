85158977170387b9c6f13fcbfbea93e21571bd9c
Tristan Ross <tristan.ross@midstall.com>
2022-10-28 11:53:45 -0700

    feat: ensure overlays apply

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -8,44 +8,48 @@
 let
   lib = import (sdkPath + "/lib/overlay.nix") nixpkgsPath;
 
-  extend-attrs = {
+  attrs-overlay = self: super: {
     inherit lib;
     path = sdkPath;
+    callPackage = path: attrs: (super.lib.callPackageWith (self // (attrs-overlay self super))) path attrs;
   };
+
+  pkgs-overlay = (self: super: {
+    gtk-layer-shell = self.callPackage ./development/libraries/gtk-layer-shell/default.nix {};
+
+    libadwaita = super.libadwaita.overrideAttrs (old: {
+      doCheck = super.stdenv.isLinux;
+      buildInputs = old.buildInputs ++ self.lib.optionals self.stdenv.isDarwin (with self.darwin.apple_sdk.frameworks; [
+        AppKit Foundation
+      ]);
+      meta.platforms = self.lib.platforms.unix;
+    });
+
+    vte = super.vte.overrideAttrs (old: {
+      mesonFlags = old.mesonFlags ++ [ "-D_b_symbolic_functions=false" ];
+      meta.broken = false;
+    });
+
+    expidus-sdk = self.callPackage ./development/tools/expidus-sdk/default.nix {};
+
+    cssparser = self.callPackage ./development/libraries/cssparser/default.nix {};
+    gxml = self.callPackage ./development/libraries/gxml/default.nix {};
+    vadi = self.callPackage ./development/libraries/vadi/default.nix {};
+
+    ntk = self.callPackage ./development/libraries/ntk/default.nix {};
+    libdevident = self.callPackage ./development/libraries/libdevident/default.nix {};
+    libtokyo = self.callPackage ./development/libraries/libtokyo/default.nix {};
+    genesis-shell = self.callPackage ./desktops/genesis-shell/default.nix {};
+    expidus-terminal = self.callPackage ./applications/terminal-emulators/expidus-terminal/default.nix {};
+  });
 
   pkgs = import (nixpkgsPath + "/default.nix") ({
     overlays = [
-      (self: super:
-        let
-          callPackage = path: attrs: (self.lib.callPackageWith (self // extend-attrs)) path attrs;
-        in (extend-attrs // {
-          gtk-layer-shell = self.callPackage ./development/libraries/gtk-layer-shell/default.nix {};
-
-          libadwaita = super.libadwaita.overrideAttrs (old: {
-            doCheck = super.stdenv.isLinux;
-            buildInputs = old.buildInputs ++ self.lib.optionals self.stdenv.isDarwin (with self.darwin.apple_sdk.frameworks; [
-              AppKit Foundation
-            ]);
-            meta.platforms = self.lib.platforms.unix;
-          });
-
-          vte = super.vte.overrideAttrs (old: {
-            mesonFlags = old.mesonFlags ++ [ "-D_b_symbolic_functions=false" ];
-            meta.broken = false;
-          });
-
-          expidus-sdk = callPackage ./development/tools/expidus-sdk/default.nix {};
-
-          cssparser = self.callPackage ./development/libraries/cssparser/default.nix {};
-          gxml = self.callPackage ./development/libraries/gxml/default.nix {};
-          vadi = self.callPackage ./development/libraries/vadi/default.nix {};
-
-          ntk = callPackage ./development/libraries/ntk/default.nix {};
-          libdevident = callPackage ./development/libraries/libdevident/default.nix {};
-          libtokyo = callPackage ./development/libraries/libtokyo/default.nix {};
-          genesis-shell = callPackage ./desktops/genesis-shell/default.nix {};
-          expidus-terminal = callPackage ./applications/terminal-emulators/expidus-terminal/default.nix {};
-        }))
+      attrs-overlay
+      pkgs-overlay
     ];
   } // args);
-in (pkgs // extend-attrs)
+
+  overlaied-attrs = attrs-overlay pkgs pkgs;
+  overlaied-pkgs = pkgs-overlay overlaied-attrs overlaied-attrs;
+in pkgs // overlaied-attrs // overlaied-pkgs
