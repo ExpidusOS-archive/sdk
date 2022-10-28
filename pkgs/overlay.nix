3c01c809b80c196355fe220c6782efbfff55f394
Tristan Ross <tristan.ross@midstall.com>
2022-10-28 11:56:40 -0700

    fix: missing argument error

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -11,36 +11,38 @@ let
   attrs-overlay = self: super: {
     inherit lib;
     path = sdkPath;
-    callPackage = path: attrs: (super.lib.callPackageWith (self // (attrs-overlay self super))) path attrs;
   };
 
-  pkgs-overlay = (self: super: {
-    gtk-layer-shell = self.callPackage ./development/libraries/gtk-layer-shell/default.nix {};
+  pkgs-overlay = (self: super:
+    let
+      callPackage = path: attrs: (super.lib.callPackageWith self) path attrs;
+    in {
+      gtk-layer-shell = self.callPackage ./development/libraries/gtk-layer-shell/default.nix {};
 
-    libadwaita = super.libadwaita.overrideAttrs (old: {
-      doCheck = super.stdenv.isLinux;
-      buildInputs = old.buildInputs ++ self.lib.optionals self.stdenv.isDarwin (with self.darwin.apple_sdk.frameworks; [
-        AppKit Foundation
-      ]);
-      meta.platforms = self.lib.platforms.unix;
-    });
+      libadwaita = super.libadwaita.overrideAttrs (old: {
+        doCheck = super.stdenv.isLinux;
+        buildInputs = old.buildInputs ++ self.lib.optionals self.stdenv.isDarwin (with self.darwin.apple_sdk.frameworks; [
+          AppKit Foundation
+        ]);
+        meta.platforms = self.lib.platforms.unix;
+      });
 
-    vte = super.vte.overrideAttrs (old: {
-      mesonFlags = old.mesonFlags ++ [ "-D_b_symbolic_functions=false" ];
-      meta.broken = false;
-    });
+      vte = super.vte.overrideAttrs (old: {
+        mesonFlags = old.mesonFlags ++ [ "-D_b_symbolic_functions=false" ];
+        meta.broken = false;
+      });
 
-    expidus-sdk = self.callPackage ./development/tools/expidus-sdk/default.nix {};
+      expidus-sdk = callPackage ./development/tools/expidus-sdk/default.nix {};
 
-    cssparser = self.callPackage ./development/libraries/cssparser/default.nix {};
-    gxml = self.callPackage ./development/libraries/gxml/default.nix {};
-    vadi = self.callPackage ./development/libraries/vadi/default.nix {};
+      cssparser = self.callPackage ./development/libraries/cssparser/default.nix {};
+      gxml = self.callPackage ./development/libraries/gxml/default.nix {};
+      vadi = self.callPackage ./development/libraries/vadi/default.nix {};
 
-    ntk = self.callPackage ./development/libraries/ntk/default.nix {};
-    libdevident = self.callPackage ./development/libraries/libdevident/default.nix {};
-    libtokyo = self.callPackage ./development/libraries/libtokyo/default.nix {};
-    genesis-shell = self.callPackage ./desktops/genesis-shell/default.nix {};
-    expidus-terminal = self.callPackage ./applications/terminal-emulators/expidus-terminal/default.nix {};
+      ntk = callPackage ./development/libraries/ntk/default.nix {};
+      libdevident = callPackage ./development/libraries/libdevident/default.nix {};
+      libtokyo = callPackage ./development/libraries/libtokyo/default.nix {};
+      genesis-shell = callPackage ./desktops/genesis-shell/default.nix {};
+      expidus-terminal = callPackage ./applications/terminal-emulators/expidus-terminal/default.nix {};
   });
 
   pkgs = import (nixpkgsPath + "/default.nix") ({
