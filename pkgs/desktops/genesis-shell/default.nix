a6c97b7f06bf60a7d77702ba36441fd096aab0ac
Tristan Ross <tristan.ross@midstall.com>
2022-10-29 17:50:53 -0700

    feat(genesis-shell): add upower on linux

--- pkgs/desktops/genesis-shell/default.nix
+++ pkgs/desktops/genesis-shell/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, gobject-introspection, dbus, vala, vadi, libtokyo, gtk-layer-shell, libpeas, libdevident, wrapGAppsHook, gsettings-desktop-schemas, expidus-sdk, networkmanager }:
+{ lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, gobject-introspection, dbus, vala, vadi, libtokyo, gtk-layer-shell, libpeas, libdevident, wrapGAppsHook, gsettings-desktop-schemas, expidus-sdk, networkmanager, upower }:
 with lib;
 stdenv.mkDerivation rec {
   pname = "genesis-shell";
@@ -15,7 +15,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ meson ninja pkg-config vala gobject-introspection wrapGAppsHook expidus-sdk ];
   buildInputs = [ vadi libdevident libtokyo libpeas dbus gsettings-desktop-schemas ]
-    ++ optionals stdenv.isLinux [ gtk-layer-shell networkmanager ];
+    ++ optionals stdenv.isLinux [ gtk-layer-shell networkmanager upower ];
   propagatedBuildInputs = buildInputs;
 
   mesonFlags = optionals stdenv.isDarwin [ "-Ddbus=disabled" "-Dx11=disabled" "-Dwayland=disabled" ];
