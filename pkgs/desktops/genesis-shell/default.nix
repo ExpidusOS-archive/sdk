eb09b864501781032232b0d2d82568a5d68448ad
Tristan Ross <tristan.ross@midstall.com>
2022-11-01 08:59:10 -0700

    chore(genesis-shell): add ibus

--- pkgs/desktops/genesis-shell/default.nix
+++ pkgs/desktops/genesis-shell/default.nix
@@ -1,4 +1,6 @@
-{ lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, gobject-introspection, dbus, vala, vadi, libtokyo, gtk-layer-shell, libpeas, libdevident, wrapGAppsHook, gsettings-desktop-schemas, expidus-sdk, networkmanager, upower, libpulseaudio }:
+{ lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, gobject-introspection, dbus, vala, vadi, libtokyo,
+  gtk-layer-shell, libpeas, libdevident, wrapGAppsHook, gsettings-desktop-schemas, expidus-sdk, networkmanager,
+  upower, libpulseaudio, ibus }:
 with lib;
 stdenv.mkDerivation rec {
   pname = "genesis-shell";
@@ -16,7 +18,7 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ meson ninja pkg-config vala gobject-introspection wrapGAppsHook expidus-sdk ];
   buildInputs = [ vadi libdevident libtokyo libpeas dbus gsettings-desktop-schemas ]
-    ++ optionals stdenv.isLinux [ gtk-layer-shell networkmanager upower libpulseaudio ];
+    ++ optionals stdenv.isLinux [ gtk-layer-shell networkmanager upower libpulseaudio ibus ];
   propagatedBuildInputs = buildInputs;
 
   mesonFlags = optionals stdenv.isDarwin [ "-Ddbus=disabled" "-Dx11=disabled" "-Dwayland=disabled" ];
