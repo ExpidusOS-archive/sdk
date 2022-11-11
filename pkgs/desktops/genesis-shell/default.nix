7660a4db1bf3be5e16abcc671de3adbc1a9ee64b
Tristan Ross <tristan.ross@midstall.com>
2022-11-10 19:15:31 -0800

    feat(genesis-shell): limit platforms

--- pkgs/desktops/genesis-shell/default.nix
+++ pkgs/desktops/genesis-shell/default.nix
@@ -29,5 +29,6 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/ExpidusOS/genesis";
     license = licenses.gpl3Only;
     maintainers = with expidus.maintainers; [ TheComputerGuy ];
+    platforms = platforms.unix;
   };
 }
