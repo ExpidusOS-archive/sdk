7bd7fd2eb886a547a60348edafc4eda316dbafe4
Tristan Ross <tristan.ross@midstall.com>
2022-11-10 19:33:06 -0800

    fix: home-manager channel

--- lib/channels/home-manager.nix
+++ lib/channels/home-manager.nix
@@ -1,4 +1,4 @@
 fetchTarball {
   url = "https://github.com/nix-community/home-manager/archive/refs/heads/release-22.05.tar.gz";
-  sha256 = "0qnkskqa6b8kby5d0262kg20rfflld4c07nfxr7651h0w8bnx04a";
+  sha256 = "0vg6x7cw2bpiga9k6nlj2n1vrm4qw84721gmlhp3j1i58v100ybc";
 }
