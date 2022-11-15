a0cc54252aa7a4e2154dc6d1d77cd97871386b37
Tristan Ross <tristan.ross@midstall.com>
2022-11-15 10:18:44 -0800

    fix: make channel input channel path

--- pkgs/top-level/make-channel.nix
+++ pkgs/top-level/make-channel.nix
@@ -2,10 +2,13 @@
   localSystem ? { system = args.system or builtins.currentSystem; },
   system ? localSystem.system,
   crossSystem ? localSystem,
-  channels ? {
-    nixpkgs = import ../../lib/channels/nixpkgs.nix;
-    sdk = ../..;
-  },
+  nixpkgs ? import ../../lib/channels/nixpkgs.nix,
+  sdk ? ../..,
   ...
 }@args:
-(import ./overlay.nix channels) (builtins.removeAttrs args [ "channels" ])
+let
+  channels = {
+    inherit nixpkgs sdk;
+  };
+in
+(import ./overlay.nix channels) (builtins.removeAttrs args [ "sdk" "nixpkgs" ])
