3664d4eebf6eea3bf0b660720983e8afeab6016d
Tristan Ross <tristan.ross@midstall.com>
2022-11-15 11:51:26 -0800

    fix: add home-manager path to make-channel

--- pkgs/top-level/make-channel.nix
+++ pkgs/top-level/make-channel.nix
@@ -3,12 +3,14 @@
   system ? localSystem.system,
   crossSystem ? localSystem,
   nixpkgs ? import ../../lib/channels/nixpkgs.nix,
+  homeManager ? import ../../lib/channels/home-manager.nix,
   sdk ? ../..,
   ...
 }@args:
 let
   channels = {
     inherit nixpkgs sdk;
+    home-manager = homeManager;
   };
 in
 (import ./overlay.nix channels) (builtins.removeAttrs args [ "sdk" "nixpkgs" ])
