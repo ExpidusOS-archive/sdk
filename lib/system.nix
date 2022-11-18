735ec8a201607b77331561fad4d95a053754c295
Tristan Ross <tross@ezoic.com>
2022-11-18 12:18:49 -0800

    feat: disable darwin if not on darwin

--- lib/system.nix
+++ lib/system.nix
@@ -1,9 +1,21 @@
 { lib }:
-rec {
+let
   current = if builtins.hasAttr "currentSystem" builtins then builtins.toString builtins.currentSystem else "x86_64-linux";
+  isSupported = value: value == true;
+  getSupportedList = list: builtins.map (system: system == current) list;
+  checkSupport = list: (builtins.length (builtins.filter isSupported (getSupportedList list))) > 0;
+in
+rec {
+  inherit current;
 
   linux = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
-  supported = linux ++ [ "aarch64-darwin" "x86_64-darwin" ];
+  darwin = [ "aarch64-darwin" "x86_64-darwin" ];
+  
+  isDarwin = checkSupport darwin;
+  isLinux = checkSupport linux;
+
+  supported = linux ++ (if isDarwin then darwin else []);
+
   forAllLinux = lib.genAttrs linux;
   forAll = lib.genAttrs supported;
 }
