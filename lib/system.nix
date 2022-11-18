13867cd24f31c0c8ee1ddd9f7007a325fb1a85b0
Tristan Ross <tross@ezoic.com>
2022-11-18 13:17:00 -0800

    fix: current being null

--- lib/system.nix
+++ lib/system.nix
@@ -1,21 +1,19 @@
 { lib }:
 let
+  host = builtins.filter builtins.isString (builtins.split "-" (builtins.getEnv "EXPIDUS_SDK_HOST"));
   currentBultin = if builtins.hasAttr "currentSystem" builtins then builtins.toString builtins.currentSystem else null;
   currentEnv =
-    let
-      env = builtins.getEnv "EXPIDUS_SDK_HOST";
-      splitten = lib.split "-" env;
-    in if env != "" then
-      "${builtins.elemAt splitten 0}-${builtins.elemAt splitten 1}"
+    if builtins.length host == 3 then
+      "${builtins.elemAt host 0}-${builtins.elemAt host 1}"
     else null;
 
-  current = if currentEnv == null then currentBultin else (if currentEnv == null then "x86_64-linux" else currentEnv);
+  current = if currentEnv == null then (if currentBultin == null then "x86_64-linux" else currentBultin) else currentEnv;
   isSupported = value: value == true;
   getSupportedList = list: builtins.map (system: system == current) list;
   checkSupport = list: (builtins.length (builtins.filter isSupported (getSupportedList list))) > 0;
 in
 rec {
-  inherit current;
+  inherit current currentBultin currentEnv;
 
   linux = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
   darwin = [ "aarch64-darwin" "x86_64-darwin" ];
