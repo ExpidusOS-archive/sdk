c35204d9e9c1b8e055e8c063d20aa7d234fc48c9
Tristan Ross <tristan.ross@midstall.com>
2022-11-19 16:36:57 -0800

    chore: update nixpkgs

--- lib/channels/nixpkgs.nix
+++ lib/channels/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/85d6b3990def7eef45f4502a82496de02a02b6e8.tar.gz";
-  sha256 = "039iya9yxyni10zc03a20ind2wbg5h18kd66yk5jb9d3q5z5rs8f";
+  url = "https://github.com/NixOS/nixpkgs/archive/52b2ac8ae18bbad4374ff0dd5aeee0fdf1aea739.tar.gz";
+  sha256 = "16dw011p3z4bgphc8ddahp3qvwj0mz266v7ldwxjl7msypxk8bmc";
 }
