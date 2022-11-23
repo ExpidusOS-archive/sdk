105e7a7cc4255b1e36000831404f399028334bf9
Tristan Ross <tross@ezoic.com>
2022-11-23 09:28:30 -0800

    feat: prevent hydra from running forever

--- pkgs/top-level/patch-packages.nix
+++ pkgs/top-level/patch-packages.nix
@@ -1,29 +1,23 @@
 { lib, config, ... }:
 pkgs: super:
 with pkgs;
+let
+  isCrossCompiling = stdenv.hostPlatform != stdenv.buildPlatform;
+in
 rec {
   inherit lib;
   path = lib.expidus.channels.sdk;
 
   nix = super.nix.overrideAttrs (old: {
-    doInstallCheck = stdenv.hostPlatform == stdenv.buildPlatform;
+    doInstallCheck = !isCrossCompiling;
   });
 
-  ninja = super.ninja.overrideAttrs (old: {
+  ninja = if isCrossCompiling then super.ninja.overrideAttrs (old: {
     src = fetchFromGitHub {
       owner = "NickCao";
       repo = "ninja";
       rev = "92330cc2320cc8aac432d80da12235abcb2bb449";
       sha256 = "G5mIIHET2Wi6RANqAyIiY+APgz7nASYOkNrkjVK14AA=";
     };
-  });
-
-  efivar = super.efivar.overrideAttrs (old: {
-    patches = (old.patches or []) ++ [
-      (fetchpatch {
-        url = "https://github.com/rhboot/efivar/commit/ca48d3964d26f5e3b38d73655f19b1836b16bd2d.patch";
-        hash = "sha256-DkNFIK4i7Eypyf2UeK7qHW36N2FSVRJ2rnOVLriWi5c=";
-      })
-    ];
-  });
+  }) else super.ninja;
 }
