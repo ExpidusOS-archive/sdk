f0b67ec431afe38220af55874ef51487074a617d
Tristan Ross <tross@ezoic.com>
2022-11-28 14:04:46 -0800

    feat: 32-bit risc-v mesa on linux

--- lib/overlay.nix
+++ lib/overlay.nix
@@ -1,3 +1,6 @@
 { nixpkgs, ... }@channels: (import (nixpkgs + "/lib/")).extend (final: prev: {
   expidus = import ./extend.nix channels;
+  platforms = prev.platforms // {
+    mesaPlatforms = prev.platforms.mesaPlatforms ++ [ "riscv32-linux" ];
+  };
 })
