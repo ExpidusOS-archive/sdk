4ebaf52efed0e32395c02ef5293ab61f1b043463
Tristan Ross <tross@ezoic.com>
2022-12-28 10:10:13 -0800

    feat(libtokyo): improve building

--- pkgs/development/libraries/libtokyo/default.nix
+++ pkgs/development/libraries/libtokyo/default.nix
@@ -1,29 +1,39 @@
-{ lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, vala, gobject-introspection, glib, sass, nodejs, vadi, gtk3, gtk4, libhandy, libadwaita, ntk, expidus-sdk, libical }:
-stdenv.mkDerivation rec {
-  pname = "libtokyo";
-  version = "0.1.0";
+{ lib, stdenv, fetchFromGitHub, fetchNpmDeps, npmHooks, meson, ninja, pkg-config, vala, gobject-introspection, glib, nodejs, vadi, gtk3, gtk4, libhandy, libadwaita, ntk, expidus-sdk, libical }:
+let
+  rev = "87ea309955c28d31b92be2a6be37526e9b44930f";
 
-  src = fetchFromGitHub {
-    owner = "ExpidusOS";
-    repo = "libtokyo";
-    rev = "dd5c3957c7330d567a2a9f5e1eb57e4e4a04b39a";
-    sha256 = "8+BOEC6YaJqUGPzVKFwO5DMnnEDPQ2gQoUiZFGMopXk=";
-    fetchSubmodules = true;
+  self = stdenv.mkDerivation {
+    pname = "libtokyo";
+    version = "0.1.0-${rev}";
+
+    src = fetchFromGitHub {
+      owner = "ExpidusOS";
+      repo = "libtokyo";
+      inherit rev;
+      sha256 = "sha256-ZLRVOObs4K1gUiMOfDw6ntdj/qyAqu3Jgqv+de0MiCs=";
+      fetchSubmodules = true;
+    };
+
+    npmDeps = fetchNpmDeps {
+      inherit (self) src;
+      name = "${self.name}-npm-deps";
+      hash = "sha256-BygDaVZV2hzE16+FRqX9Zcyk2DKnweftnY/7yAEXzSs=";
+    };
+
+    outputs = [ "out" "dev" "devdoc" ];
+    doChecks = true;
+
+    nativeBuildInputs = [ meson ninja pkg-config vala gobject-introspection nodejs expidus-sdk npmHooks.npmConfigHook ];
+    buildInputs = [ vadi gtk3 gtk4 libhandy libadwaita ntk libical ];
+    propagatedBuildInputs = self.buildInputs;
+
+    mesonFlags = [ "-Dntk=enabled" "-Dgtk4=enabled" "-Dgtk3=enabled" "-Dnodejs=enabled" ];
+
+    meta = with lib; {
+      description = "A libadwaita wrapper for ExpidusOS with Tokyo Night's styling";
+      homepage = "https://github.com/ExpidusOS/libtokyo";
+      license = licenses.gpl3Only;
+      maintainers = with expidus.maintainers; [ TheComputerGuy ];
+    };
   };
-
-  outputs = [ "out" "dev" "devdoc" ];
-  doChecks = true;
-
-  nativeBuildInputs = [ meson ninja pkg-config vala gobject-introspection sass nodejs expidus-sdk ];
-  buildInputs = [ vadi gtk3 gtk4 libhandy libadwaita ntk libical ];
-  propagatedBuildInputs = buildInputs;
-
-  mesonFlags = ["-Dntk=enabled" "-Dgtk4=enabled" "-Dgtk3=enabled" "-Dnodejs=disabled"];
-
-  meta = with lib; {
-    description = "A libadwaita wrapper for ExpidusOS with Tokyo Night's styling";
-    homepage = "https://github.com/ExpidusOS/libtokyo";
-    license = licenses.gpl3Only;
-    maintainers = with expidus.maintainers; [ TheComputerGuy ];
-  };
-}
+in self
