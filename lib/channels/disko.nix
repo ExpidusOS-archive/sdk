02f702f13b612365c7054ed62f9b0887813b14fe
Tristan Ross <tristan.ross@midstall.com>
2023-01-01 16:09:04 -0800

    chore: set disko hash

--- lib/channels/disko.nix
+++ lib/channels/disko.nix
@@ -1,4 +1,4 @@
 fetchTarball {
   url = "https://github.com/nix-community/disko/archive/981160e9cfb73a9fc9f7eddfe193953fc784cdfb.tar.gz";
-  sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+  sha256 = "0v5wsmrj2b9a6kxd5j8k24qkizvhm52lycafnhwv1gmrq949p5s2";
 }
