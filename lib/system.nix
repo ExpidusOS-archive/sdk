28e38353a89dc09d32e821f0dada77d1b2e9f34f
Tristan Ross <tristan.ross@midstall.com>
2023-01-03 17:28:18 -0800

    fix(firefox): missing attribute

--- lib/system.nix
+++ lib/system.nix
@@ -29,7 +29,7 @@ let
   ] lib.platforms.linux;
   defaultExtra = [
     {
-      name = "wasm32-wasi";
+      name = "wasm32";
       value = {
         config = "wasm32-unknown-wasi";
         useLLVM = true;
