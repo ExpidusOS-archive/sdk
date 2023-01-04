bda22528b24eda9fc3843bc715c9ee4cab6902e8
Tristan Ross <tristan.ross@midstall.com>
2023-01-03 16:48:38 -0800

    feat: remove aarch64 from pkgs cross

--- lib/overlay.nix
+++ lib/overlay.nix
@@ -1,11 +1,14 @@
 { nixpkgs, ... }@channels: (import (nixpkgs + "/lib/")).extend (final: prev: {
   expidus = import ./extend.nix channels;
   systems = prev.systems // {
-    examples = builtins.listToAttrs (builtins.map (system: {
-      name = system;
-      value = {
-        inherit system;
-      };
-    }) final.expidus.system.possible);
+    examples =
+      let
+        systems = final.lists.subtractLists (builtins.map (name: "${name}-darwin") [ "aarch64" "x86_64" ]) final.expidus.system.possible;
+      in builtins.listToAttrs (builtins.map (system: {
+        name = system;
+        value = {
+          inherit system;
+        };
+      }) systems);
   };
 })
