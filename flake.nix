9534032f37f8bda1ea446f0b1bba3ab0f158ebed
Tristan Ross <tross@ezoic.com>
2023-01-04 11:35:58 -0800

    feat: enable fallback

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
     substituters = [ "https://cache.nixos.org" "https://cache.garnix.io" ];
     trusted-substituters = substituters;
+    fallback = true;
   };
 
   inputs.utils.url = github:numtide/flake-utils;
