f74b802b6c375ca4433c8dc44545bff61fbe38d5
Tristan Ross <tristan.ross@midstall.com>
2023-01-13 18:37:10 -0800

    fix(pkgs/libvpx): missing cpu arch

--- pkgs/development/libraries/libvpx/default.nix
+++ pkgs/development/libraries/libvpx/default.nix
@@ -182,7 +182,7 @@ stdenv.mkDerivation rec {
     # See all_platforms: https://github.com/webmproject/libvpx/blob/master/configure
     # Darwin versions: 10.4=8, 10.5=9, 10.6=10, 10.7=11, 10.8=12, 10.9=13, 10.10=14
     (let
-      prag = "${stdenv.hostPlatform.parsed.cpu.arch}-${stdenv.hostPlatform.parsed.kernel.name}${
+      prag = "${stdenv.hostPlatform.parsed.cpu.arch or stdenv.hostPlatform.parsed.cpu.name}-${stdenv.hostPlatform.parsed.kernel.name}${
             if stdenv.hostPlatform.isDarwin then
               if      stdenv.hostPlatform.osxMinVersion == "10.10" then "14"
               else if stdenv.hostPlatform.osxMinVersion == "10.9"  then "13"
