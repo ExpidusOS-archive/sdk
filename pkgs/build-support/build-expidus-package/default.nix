08cb64e91df3b396c9183c8e1df2ddb0b4d5b144
Tristan Ross <tristan.ross@midstall.com>
2023-01-15 12:45:36 -0800

    feat(build-expidus-package): working to build

--- pkgs/build-support/build-expidus-package/default.nix
+++ pkgs/build-support/build-expidus-package/default.nix
@@ -1,5 +1,6 @@
 { lib, clang14Stdenv, flutter, expidus }:
-{ src,
+{ exec,
+  src,
   postUnpack ? "",
   runtime ? expidus.runtimes,
   plugins ? {},
@@ -8,7 +9,11 @@
   passthru ? {},
   meta ? {}
 }@args:
-with lib; clang14Stdenv.mkDerivation ({
+with lib;
+let
+  platform = "linux"; # FIXME: use targetPlatform
+  arch = "x64"; # FIXME: use targetPlatform
+in clang14Stdenv.mkDerivation ({
   inherit src;
 
   nativeBuildInputs = [ flutter ] ++ nativeBuildInputs;
@@ -22,6 +27,22 @@ with lib; clang14Stdenv.mkDerivation ({
     echo "$flutterPlugins" >.flutter-plugins
   '' ++ (optionalString (postUnpack != "" && postUnpack != null) postUnpack);
 
+  configurePhase = ''
+    flutter pub get
+  '';
+
+  buildPhase = ''
+    flutter build ${platform}
+  '';
+
+  installPhase = ''
+    mkdir -p $out/lib/$name
+    cp -r build/${platform}/${arch}/bundle $out/lib/$name
+
+    wrapProgram $out/lib/$name/${exec} \
+      --suffix LD_LIBRARY_PATH : "$out/lib/$name/lib"
+  '';
+
   passthru = passthru // {
     expidus-runtime = runtime;
   };
@@ -29,4 +50,4 @@ with lib; clang14Stdenv.mkDerivation ({
   meta = {
     platforms = builtins.attrValues (lib.expidus.system.default.forAllSystems (system: _: system));
   } // meta;
-} // (builtins.removeAttrs args [ "nativeBuildInputs" "buildInputs" "meta" "src" "runtime" "postUnpack" "plugins" ]))
+} // (builtins.removeAttrs args [ "nativeBuildInputs" "buildInputs" "meta" "src" "runtime" "postUnpack" "plugins" "exec" ]))
