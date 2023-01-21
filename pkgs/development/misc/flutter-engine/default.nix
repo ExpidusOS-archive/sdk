468754a8ba51f5ca43cb0476906e97b15dd47f35
tross <tross@ezoic.com>
2023-01-20 16:38:01 -0800

    feat(flutter-engine): merge src

--- pkgs/development/misc/flutter-engine/default.nix
+++ pkgs/development/misc/flutter-engine/default.nix
@@ -8,97 +8,85 @@ let
 
   toolchainArch = if hostPlatform.isx86_64 then "amd64" else if hostPlatform.isAarch64 then "aarch64" else throws "Unsupported platform";
   interpreter = "ld-linux-${hostPlatform.parsed.cpu.arch}.so.2";
-
-  src = stdenvNoCC.mkDerivation rec {
-    pname = "flutter-engine-src";
-    inherit version;
-
-    SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
-
-    src = fetchFromGitHub {
-      owner = "flutter";
-      repo = "engine";
-      rev = version;
-      sha256 = "sha256-5mKxrm+hvrXeLPjSYihbfd9C5LU1c2IZfqaSy6p/6Vo=";
-      leaveDotGit = true;
-    };
-
-    toolchain = fetchurl {
-      url = "https://commondatastorage.googleapis.com/chrome-linux-sysroot/toolchain/79a7783607a69b6f439add567eb6fcb48877085c/debian_sid_${toolchainArch}_sysroot.tar.xz";
-      sha256 = "sha256-+jooJ4YcWHUfD5AT+Yhe0nYQNBl7IvJl2dRc+d3Yl+g=";
-    };
-
-    nativeBuildInputs = [ openssh git gclient-wrapped clang-tools makeWrapper patchelf python3 pkg-config ];
-
-    gclientFile = writeText "gclient" ''
-      solutions = [
-        {
-          "managed": False,
-          "name": "src/flutter",
-          "url": "file://${src}",
-          "custom_deps": {
-            ${concatStringsSep ",\n" (attrValues (mapAttrs (name: src: "\"${name}\": \"file://${src}\"") flutter-deps))}
-          },
-          "deps_file": "DEPS",
-          "safesync_url": "",
-        },
-      ]
-    '';
-
-    dontUnpack = true;
-    dontConfigure = true;
-    dontInstall = true;
-
-    binaryFixes = [
-      "src/third_party/dart/tools/sdks/dart-sdk/bin/dart"
-      "src/flutter/third_party/gn/gn"
-    ];
-
-    buildPhase = ''
-      mkdir -p $out/src
-      cp --no-preserve=ownership $gclientFile $out/.gclient
-      cd $out
-      gclient sync --nohooks --no-history
-      find src -name '.git' -exec chmod 0755 {} \;
-      find src -name '.git' -exec rm -rf {} \; || true
-
-      for bin in $binaryFixes; do
-        chmod 0755 $bin
-        patchelf --set-interpreter ${stdenv.cc.libc}/lib/${interpreter} $bin
-      done
-
-      for bin in $(find src/buildtools/ -type l); do
-        chmod 0755 $bin
-        patchelf --set-interpreter ${stdenv.cc.libc}/lib/${interpreter} $bin || true
-      done
-
-      sed -i '1 s|^.*$|#!${gnumake}/bin/make -f|' src/third_party/harfbuzz/src/update-unicode-tables.make
-
-      mkdir -p src/build/linux/debian_sid_${toolchainArch}-sysroot
-      tar -xf $toolchain -C src/build/linux/debian_sid_${toolchainArch}-sysroot
-
-      rm -rf .cipd
-    '';
-
-    outputHashAlgo = "sha256";
-    outputHashMode = "recursive";
-    outputHash = "sha256-9pwV+RuqNo+WHZviw+p8kMALqDich531xx1vN5AmBSg=";
-  };
-in stdenv.mkDerivation {
+in stdenvNoCC.mkDerivation rec {
   pname = "flutter-engine";
-  inherit version src;
+  inherit version;
 
-  nativeBuildInputs = [ patchelf python3 pkg-config ];
+  SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
 
-  postUnpack = ''
-    python3 src/third_party/dart/tools/generate_package_config.py
-    python3 src/third_party/dart/tools/generate_sdk_version_file.py
-    python3 src/tools/remove_stale_pyc_files.py src/tools
+  src = fetchFromGitHub {
+    owner = "flutter";
+    repo = "engine";
+    rev = version;
+    sha256 = "sha256-5mKxrm+hvrXeLPjSYihbfd9C5LU1c2IZfqaSy6p/6Vo=";
+    leaveDotGit = true;
+  };
+
+  toolchain = fetchurl {
+    url = "https://commondatastorage.googleapis.com/chrome-linux-sysroot/toolchain/79a7783607a69b6f439add567eb6fcb48877085c/debian_sid_${toolchainArch}_sysroot.tar.xz";
+    sha256 = "sha256-+jooJ4YcWHUfD5AT+Yhe0nYQNBl7IvJl2dRc+d3Yl+g=";
+  };
+
+  nativeBuildInputs = [ openssh git gclient-wrapped clang-tools makeWrapper patchelf python3 pkg-config ];
+
+  gclientFile = writeText "gclient" ''
+    solutions = [
+      {
+        "managed": False,
+        "name": "src/flutter",
+        "url": "file://${src}",
+        "custom_deps": {
+          ${concatStringsSep ",\n" (attrValues (mapAttrs (name: src: "\"${name}\": \"file://${src}\"") flutter-deps))}
+        },
+        "deps_file": "DEPS",
+        "safesync_url": "",
+      },
+    ]
+  '';
+
+  binaryFixes = [
+    "src/third_party/dart/tools/sdks/dart-sdk/bin/dart"
+    "src/flutter/third_party/gn/gn"
+  ];
+
+  unpackPhase = ''
+    runHook preUnpack
+
+    mkdir -p src
+    cp --no-preserve=ownership $gclientFile .gclient
+    cd $out
+    gclient sync --nohooks --no-history
+    find src -name '.git' -exec chmod 0755 {} \;
+    find src -name '.git' -exec rm -rf {} \; || true
+
+    for bin in $binaryFixes; do
+      chmod 0755 $bin
+      patchelf --set-interpreter ${stdenv.cc.libc}/lib/${interpreter} $bin
+    done
+
+    for bin in $(find src/buildtools/ -type l); do
+      chmod 0755 $bin
+      patchelf --set-interpreter ${stdenv.cc.libc}/lib/${interpreter} $bin || true
+    done
+
+    sed -i '1 s|^.*$|#!${gnumake}/bin/make -f|' src/third_party/harfbuzz/src/update-unicode-tables.make
+
+    mkdir -p src/build/linux/debian_sid_${toolchainArch}-sysroot
+    tar -xf $toolchain -C src/build/linux/debian_sid_${toolchainArch}-sysroot
+
+    runHook postUnpack
   '';
 
   configurePhase = ''
     runHook preConfigure
+      
+    python3 src/third_party/dart/tools/generate_package_config.py
+    python3 src/third_party/dart/tools/generate_sdk_version_file.py
+    python3 src/tools/remove_stale_pyc_files.py src/tools
+    python3 src/flutter/tools/pub_get_offline.py
+
     ./src/flutter/tools/gn --no-build-glfw-shell --prebuilt-dart-sdk --embedder-for-target --no-goma
+
     runHook postConfigure
   '';
 
@@ -127,4 +115,8 @@ in stdenv.mkDerivation {
     cp src/out/host_debug/libflutter_engine.so $out/lib
     cp src/out/host_debug/icudtl.dat $out/lib/flutter
   '';
+
+  outputHashAlgo = "sha256";
+  outputHashMode = "recursive";
+  outputHash = "sha256-9pwV+RuqNo+WHZviw+p8kMALqDich531xx1vN5AmBSg=";
 }
