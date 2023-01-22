945f2f0066476191635c316225c6f6a4f2941437
Tristan Ross <tristan.ross@midstall.com>
2023-01-21 22:12:22 -0800

    chore: update flake

--- pkgs/development/misc/flutter-engine/default.nix
+++ pkgs/development/misc/flutter-engine/default.nix
@@ -3,7 +3,7 @@ with lib;
 let
   config = {
     debug = "sha256-rwLH6yBd/h4UNijAs4w2UF6rMJnRv2qylzyRAqAa4Cc=";
-    profile = fakeHash;
+    profile = "sha256-XENIl+Hv+xg6b3C3Wo43j0rf7vGYzSf4wl1+wKuO2Gw=";
     release = fakeHash;
     jit_release = fakeHash;
   };
