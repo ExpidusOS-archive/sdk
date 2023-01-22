09a3910ab0cc54af6266c2a8f9f3aa5a92a24e7c
Tristan Ross <tristan.ross@midstall.com>
2023-01-21 22:42:19 -0800

    feat(flutter-engine): working

--- pkgs/development/misc/flutter-engine/default.nix
+++ pkgs/development/misc/flutter-engine/default.nix
@@ -4,8 +4,8 @@ let
   config = {
     debug = "sha256-rwLH6yBd/h4UNijAs4w2UF6rMJnRv2qylzyRAqAa4Cc=";
     profile = "sha256-XENIl+Hv+xg6b3C3Wo43j0rf7vGYzSf4wl1+wKuO2Gw=";
-    release = fakeHash;
-    jit_release = fakeHash;
+    release = "sha256-gpja2/jmWkMpq4BCYTJQwLxVaKy/Z8m/Y2FYv5p0f4A=";
+    jit_release = "sha256-5/NYM6dtM4ESE2xO20bom5Jg00AWsD2kNLlJBAyNy5Y=";
   };
   mkPackage = callPackage ./package.nix {};
 in builtins.mapAttrs (runtimeMode: sha256: mkPackage { inherit runtimeMode sha256; }) config
