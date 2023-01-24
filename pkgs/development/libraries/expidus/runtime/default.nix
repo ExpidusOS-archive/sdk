bbb9142e23bb263a4b4b89afccf9923e8133fd18
tross <tross@ezoic.com>
2023-01-23 16:43:39 -0800

    feat: remove unneeded packages

--- pkgs/development/libraries/expidus/runtime/default.nix
+++
@@ -1,8 +0,0 @@
-{ lib, callPackage }:
-with lib;
-let
-  pkg = callPackage ./package.nix {};
-in pkg {
-  rev = "53cff094977eca64d590ce68785016e3c2ffad11";
-  sha256 = "sha256-/CwZq75QWLNxHd+zi6XeNv6+dmn5xCKQcZ6DuMw70ys=";
-}
--- pkgs/top-level/expidus-packages.nix
+++ pkgs/top-level/expidus-packages.nix
@@ -1,13 +1,3 @@
 { callPackage }:
 rec {
-  sdk = callPackage ../development/misc/expidus/sdk/default.nix {};
-  buildPackage = callPackage ../build-support/build-expidus-package/default.nix {};
-  runtime = callPackage ../development/libraries/expidus/runtime/default.nix {};
-
-  runtime-example = buildPackage {
-    pname = "expidus-runtime-example";
-    inherit (runtime) version;
-    src = "${runtime.src}/example";
-    vendorSha256 = "sha256-FxJmIoycLUQPlR2qV9vue2NKq3eNW6Tq4awSGqbwsis=";
-  };
 }
