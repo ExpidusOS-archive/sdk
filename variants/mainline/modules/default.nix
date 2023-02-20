d9c3458c94cf30d54e7996cc244084806949de67
Tristan Ross <tristan.ross@midstall.com>
2023-02-20 04:21:23 -0800

    feat(mainline): wip

--- variants/mainline/modules/default.nix
+++ variants/mainline/modules/default.nix
@@ -4,4 +4,6 @@
   "${nixpkgs}/nixos/modules/misc/lib.nix"
   "${nixpkgs}/nixos/modules/misc/meta.nix"
   "${nixpkgs}/nixos/modules/misc/nixpkgs.nix"
+  "${nixpkgs}/nixos/modules/system/etc/etc.nix"
+  "${nixpkgs}/nixos/modules/system/build.nix"
 ]
