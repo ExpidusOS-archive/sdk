d6bb3057e0ff453e8cc8e5692cf29b7d76fec448
Tristan Ross <tristan.ross@midstall.com>
2023-02-26 00:23:24 -0800

    chore(libexpidus): bump

--- pkgs/development/libraries/expidus/libexpidus/default.nix
+++ pkgs/development/libraries/expidus/libexpidus/default.nix
@@ -2,6 +2,6 @@
 let
   mkPackage = callPackage ./package.nix { inherit stdenv; };
 in mkPackage {
-  rev = "940cd680533efc25a80b7ae902d8d4b894f0a770";
-  sha256 = "sha256-pIzgXEKc8oh+Dj4yYrtJw2cqtgRdY7lv8r6H3Gcw5Tg=";
+  rev = "16936f3cf4cc2108973c898b845c9b3bf23c26a5";
+  sha256 = "sha256-tLuZxXFsemolHGKw4Xxqcnss3obJcdbt90Qy1UWQ7Cg=";
 }
