653f35d48b1e7589573e9205ba29bc467f209327
Tristan Ross <tristan.ross@midstall.com>
2023-02-25 18:42:36 -0800

    chore(expidus-libventify): bump

--- pkgs/development/libraries/expidus/libvenfig/default.nix
+++ pkgs/development/libraries/expidus/libvenfig/default.nix
@@ -2,6 +2,6 @@
 let
   mkPackage = callPackage ./package.nix { inherit stdenv; };
 in mkPackage {
-  rev = "94eba3f78df88f04a793cdee8e9c54ca7e2b87ef";
-  sha256 = "sha256-boOAT/BLf1s+RRybzH+qSbgxg34Wc1CJBiKJCDtJxJo=";
+  rev = "608da0047791169dde896704e90dddceb5fb5a97";
+  sha256 = "sha256-j/uD+kyyAL27TCezxyunzR0blBl3BVgMu3xbDMdQtNE=";
 }
