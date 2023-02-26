2cc73af533142c88284ce6dde29b87eb80c43ae5
Tristan Ross <tristan.ross@midstall.com>
2023-02-26 01:35:19 -0800

    chore(expidus-neutron): bump

--- pkgs/development/libraries/expidus/neutron/default.nix
+++ pkgs/development/libraries/expidus/neutron/default.nix
@@ -2,7 +2,7 @@
 callPackage ./package.nix {
   inherit stdenv isWASM;
 } {
-  rev = "2874a61abb61e17b22297f1d05e884df701ca80c";
-  sha256 = "sha256-B3gIko3dmygl/ZuHaKUFWVjVP5aXA+93Nb58n1103eE=";
+  rev = "4e9828fec1d253480e9e47c603aa9384c0ad26bb";
+  sha256 = "sha256-mdfRHvrH0ng/8mKP8WQYKixXMOQSVNMoTs0X7EZRhRQ=";
   inherit isWASM;
 }
