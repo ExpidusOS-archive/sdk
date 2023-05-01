1365d043d384ee9ec2f9eafa7ccd19bdee498d94
Tristan Ross <tristan.ross@midstall.com>
2023-04-30 17:20:42 -0700

    refactor(expidus-neutron): use latest version

--- pkgs/development/libraries/expidus/neutron/default.nix
+++ pkgs/development/libraries/expidus/neutron/default.nix
@@ -2,6 +2,6 @@
 callPackage ./package.nix {
   inherit stdenv zig;
 } {
-  rev = "bb344783dd9e7ba8621745c9298074a8caeabd2f";
-  sha256 = "sha256-kS2JnQ2H0gdP9UUrBfpFQmTdIYPFa++qf+mNuzKsm8c=";
+  rev = "f15dedc4f9b74b6ae9074d6b3cd9c1a4102e1135";
+  sha256 = "sha256-ew6TmAYdXBBXnrYlSxu1jOE17EN0axLDoptlfoDqYOI=";
 }
--- pkgs/development/libraries/expidus/neutron/package.nix
+++ pkgs/development/libraries/expidus/neutron/package.nix
@@ -1,12 +1,20 @@
 { lib,
+  buildFHSUserEnv,
   fetchFromGitHub,
-  fetchFromGitLab,
+  writeShellScriptBin,
   makeWrapper,
   stdenv,
   buildPlatform,
   buildPackages,
   targetPlatform,
-  flutter-engine,
+  wayland,
+  wayland-protocols,
+  wlroots,
+  pixman,
+  libxkbcommon,
+  libdrm,
+  mesa,
+  libglvnd,
   zig
 }@pkgs:
 with lib;
@@ -22,7 +30,6 @@ let
     zig ? pkgs.zig,
     enableDocs ? true,
     vendorOverride ? {},
-    engineType ? "release",
     buildType ? "ReleaseFast",
     sha256 ? fakeHash,
     target ? "${targetPlatform.system}-gnu"
@@ -39,83 +46,118 @@ let
         zig = pkgs.zig;
         enableDocs = true;
         vendorOverride = {};
-        engineType = "release";
         buildType = "ReleaseFast";
         sha256 = fakeHash;
         target = "${targetPlatform.system}-gnu";
       } // args;
 
       vendor = {
-        "os-specific/linux/libs/drm" = fetchFromGitLab {
-          domain = "gitlab.freedesktop.org";
-          owner = "mesa";
-          repo = "drm";
-          rev = "ee558cea20d1f9d822fe1a28e97beaf365bf9d38";
-          sha256 = "sha256-Ju9QbbsjDW4f9lFfClqlWcqodNMvfD4hjDkSOy6kb7k=";
+        "bindings/vulkan-zig" = fetchFromGitHub {
+          owner = "Snektron";
+          repo = "vulkan-zig";
+          rev = "02939ff0266678643e8f32ea430bbef3335705b8";
+          sha256 = "sha256-QapGlkgPNhIqniYemcCNCJtNogT83Rn+IoUVqC1Jtto=";
         };
-        "os-specific/linux/libs/wayland" = fetchFromGitLab {
-          domain = "gitlab.freedesktop.org";
-          owner = "wayland";
-          repo = "wayland";
-          rev = "8135e856ebd79872f886466e9cee39affb7d9ee8";
-          sha256 = "sha256-nvNDONDdpoYNDD5q29IisvUY2lHsEcgJYGJUWbhpijs=";
+        "bindings/zig-flutter" = fetchFromGitHub {
+          owner = "ExpidusOS";
+          repo = "zig-flutter";
+          rev = "256de1bf11ba5ea20740a575a1b1e078e547abad";
+          fetchSubmodules = true;
+          sha256 = "sha256-JHmwuBl1qFrihRSONK5wYbd+jQJJQxQEB0ZwpCQzNp8=";
         };
-        "os-specific/linux/libs/wayland-protocols" = fetchFromGitLab {
-          domain = "gitlab.freedesktop.org";
-          owner = "wayland";
-          repo = "wayland-protocols";
-          rev = "e631010ab7b96988e7c64c24b7d90f64717eaeee";
-          sha256 = "sha256-eS7nurCjYetDNQORYIVITE93JW4KUqJv/GBEoe/HUkw=";
+        "bindings/zig-pixman" = fetchFromGitHub {
+          owner = "ifreund";
+          repo = "zig-pixman";
+          rev = "4a49ba13eb9ebb0c0f991de924328e3d615bf283";
+          sha256 = "sha256-2iuK2DVu5w29eDHnlVyWt4d6as6gjVqhg+1/TxRmkZ8=";
         };
-        "os-specific/linux/zig/zig-wayland" = fetchFromGitHub {
+        "bindings/zig-wayland" = fetchFromGitHub {
           owner = "ExpidusOS";
           repo = "zig-wayland";
-          rev = "ba8cee50a8f761f0aef7922a2f0747c37ca428b1";
-          sha256 = "sha256-Cotkqv5v1hJh3NBbi4I2dOFKZjG7kCdLttGf0L1f6Ew=";
+          rev = "95e8afad008b5c443f6420dde46e92a5709a3481";
+          sha256 = "sha256-Boq1OqGBcDvfhTCdk7eWKrNVfcPCBS7QOt0ETJYZMFo=";
         };
-        "third-party/libs/expat" = fetchFromGitHub {
-          owner = "libexpat";
-          repo = "libexpat";
-          rev = "654d2de0da85662fcc7644a7acd7c2dd2cfb21f0";
-          sha256 = "sha256-nX8VSlqpX/SVE4fpPLOzj3s/D3zmTC9pObIYfkQq9RA=";
+        "bindings/zig-wlroots" = fetchFromGitHub {
+          owner = "swaywm";
+          repo = "zig-wlroots";
+          rev = "c4cdb08505de19f6bfbf8e1825349b80c7696475";
+          sha256 = "sha256-1m5QBroCOOFmoP51S2zZafNhUFWiwr20ETiKCp5uKHc=";
         };
-        "third-party/libs/hwdata" = fetchFromGitHub {
-          owner = "vcrhonek";
-          repo = "hwdata";
-          rev = "0e25d93ac6433791edbb9d28b3f8eae0cf5e46ff";
-          sha256 = "sha256-+9UyF4tcy5cJPjbyQ2RuWVJkBsZut+YX2ncUJIqIQZo=";
+        "bindings/zig-xkbcommon" = fetchFromGitHub {
+          owner = "ifreund";
+          repo = "zig-xkbcommon";
+          rev = "bfd1f97c277c32fddb77dee45979d2f472595d19";
+          sha256 = "sha256-2fEMbN86alYN6JLRVL/0dbgp/Qo5M3uXKc5RGIUGGC4=";
         };
-        "third-party/libs/libdisplay-info" = fetchFromGitLab {
-          domain = "gitlab.freedesktop.org";
-          owner = "emersion";
-          repo = "libdisplay-info";
-          rev = "92b031749c0fe84ef5cdf895067b84a829920e25";
-          sha256 = "sha256-7t1CoLus3rPba9paapM7+H3qpdsw7FlzJsSHFwM/2Lk=";
+        "third-party/zig/libxev" = fetchFromGitHub {
+          owner = "mitchellh";
+          repo = "libxev";
+          rev = "e7d4e6dfd208b4d90715766f92aeaf0163e4bdd9";
+          sha256 = "sha256-CPeuKZxJhRFuqjwQWfO0K0Q7umGi2wozznSQR4RwjOk=";
         };
-        "third-party/libs/libffi" = fetchFromGitHub {
-          owner = "libffi";
-          repo = "libffi";
-          rev = "f08493d249d2067c8b3207ba46693dd858f95db3";
-          sha256 = "sha256-98WtmJLAdI6cpQAOBTCz7OyZl7um9XAzkRVL12NZ1mc=";
+        "third-party/zig/s2s" = fetchFromGitHub {
+          owner = "ziglibs";
+          repo = "s2s";
+          rev = "b39cb2a75eb4b695f36dfbb8ee26bc5688987399";
+          sha256 = "sha256-oY0QDpCZ8iG+v7F7p0oTrXF69/QjTAdphH7f4XQ9mvw=";
         };
         "third-party/zig/zig-clap" = fetchFromGitHub {
           owner = "Hejsil";
           repo = "zig-clap";
-          rev = "cb13519431b916c05c6c783cb0ce3b232be5e400";
-          sha256 = "sha256-ej4r5LGsTqhQkw490yqjiTOGk+jPMJfUH1b/eUmvt20=";
+          rev = "ab69ef2db44b6c4b7f00283d52d38fbe71d16c42";
+          sha256 = "sha256-wQAN6IRKp3l4oSw2qhvsqzsK+gjF0W+QDiGbq9PB530=";
         };
       } // args'.vendorOverride;
 
       version = "git+${builtins.substring 0 7 args'.rev}${optionalString (branch != "master") "-${args'.branch}"}";
+
+      buildInputs = [
+        wayland
+        wayland-protocols
+        wlroots
+        pixman
+        libxkbcommon
+        libdrm
+        mesa
+        mesa.osmesa
+        libglvnd
+      ];
+
+      zig-wrapped = writeShellScriptBin "expidus-neutron-zig" ''
+        export PKG_CONFIG_PATH=${concatMapStringsSep ":" (pkg: (concatMapStringsSep ":" (output: concatMapStringsSep ":" (path: "${pkg.${output}.outPath}${path}") [ "/lib/pkgconfig" "/share/pkgconfig" ]) pkg.outputs)) buildInputs}
+        exec ${zig}/bin/zig $@
+      '';
+
+      fhsEnv = buildFHSUserEnv {
+        name = "expidus-neutron";
+
+        targetPkgs = pkgs:
+          (with buildPackages; [
+            zig
+            ninja
+            zlib
+            git
+            curl
+            pkg-config
+          ]);
+
+        runScript = "${zig-wrapped}/bin/${zig-wrapped.name}";
+      };
     in stdenv.mkDerivation {
       pname = "expidus-neutron";
-      inherit version;
+      inherit version buildInputs;
       inherit (args') src;
 
       outputs = [ "out" ]
         ++ optional (args'.enableDocs) "devdocs";
 
-      nativeBuildInputs = [ args'.zig ];
+      strictDeps = true;
+      depsBuildBuild = [ buildPackages.pkg-config ];
+
+      nativeBuildInputs = with buildPackages; [
+        wayland-scanner
+        pkg-config
+      ];
 
       configurePhase = ''
         ${concatStrings (attrValues (mapAttrs (path: src: ''
@@ -127,10 +169,7 @@ let
 
       buildFlags = [
         "-Dtarget=${args'.target}"
-        "-Dflutter-engine=${flutter-engine.${args'.engineType}}/lib/flutter/out/${args'.engineType}"
-        "-Dhost-dynamic-linker=${buildPackages.stdenv.cc.libc}/lib/ld-linux-${replaceStrings ["_"] ["-"] buildPlatform.parsed.cpu.name}.so.2"
-      ] ++ optional (args'.target == "${targetPlatform.system}-gnu")
-        "-Dtarget-dynamic-linker=${stdenv.cc.libc}/lib/ld-linux-${replaceStrings ["_"] ["-"] targetPlatform.parsed.cpu.name}.so.2";
+      ];
 
       dontBuild = true;
 
@@ -138,7 +177,7 @@ let
         export XDG_CACHE_HOME=$NIX_BUILD_TOP/.cache
         mkdir -p $out/lib
 
-        zig build $buildFlags --prefix $out \
+        ${fhsEnv}/bin/${fhsEnv.name} build $buildFlags --prefix $out \
           --prefix-lib-dir $out/lib \
           --cache-dir $out/zig-cache
 
@@ -156,9 +195,9 @@ let
         rm -rf $out/zig-cache/zig-wayland
         find $out/zig-cache/o -type f -not -name '*.so*' -delete
 
-        if [[ -f $out/bin/neutron-runner ]]; then
+        if [[ -f $out/bin/neutron ]]; then
           source ${makeWrapper.outPath}/nix-support/setup-hook
-          wrapProgram $out/bin/neutron-runner \
+          wrapProgram $out/bin/neutron \
             --prefix LD_LIBRARY_PATH : "$out/lib"
         fi
       '';
