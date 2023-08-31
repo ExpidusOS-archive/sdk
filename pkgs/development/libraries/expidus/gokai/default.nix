39fa9bcbf94a9ffc1e510462e2cabb37eec66e9c
Tristan Ross <tristan.ross@midstall.com>
2023-08-31 14:23:02 -0700

    feat(gokai): adding aarch64 support

--- pkgs/development/libraries/expidus/gokai/default.nix
+++ pkgs/development/libraries/expidus/gokai/default.nix
@@ -1,6 +1,9 @@
 { lib,
   stdenv,
   fetchFromGitHub,
+  fetchurl,
+  targetPlatform,
+  runCommand,
   wayland-scanner,
   meson,
   ninja,
@@ -28,9 +31,39 @@
   accountsservice,
   upower,
   flutter-engine
-}:
+}@pkgs:
 let
   rev = "6a11f0553efc79ed85199e7fa738718489b6a8dd";
+
+  flutter-engineHash = "cdbeda788a293fa29665dc3fa3d6e63bd221cb0d";
+  flutter-engines = builtins.mapAttrs
+    (name: sha256: runCommand "flutter-engine-${flutter-engineHash}" {
+      src = fetchurl {
+        url = "https://github.com/ardera/flutter-ci/releases/download/engine%2F${flutter-engineHash}/${name}.tar.xz";
+        inherit sha256;
+      };
+    } ''
+      mkdir $NIX_BUILD_TOP/source
+      tar xvf $src -C $NIX_BUILD_TOP/source
+
+      mkdir -p $out/src/out/host_{release,debug,profile}
+      cp $NIX_BUILD_TOP/source/gen_snapshot_linux_*_profile $out/src/out/host_profile/gen_snapshot
+      cp $NIX_BUILD_TOP/source/gen_snapshot_linux_*_release $out/src/out/host_release/gen_snapshot
+      cp $NIX_BUILD_TOP/source/gen_snapshot_linux_*_release $out/src/out/host_debug/gen_snapshot
+
+      for target in profile debug release; do
+        cp $NIX_BUILD_TOP/source/libflutter_engine.so.$target $out/src/out/host_$target/libflutter_engine.so
+        cp $NIX_BUILD_TOP/source/icudtl.dat $out/src/out/host_$target/icudtl.dat
+        cp $NIX_BUILD_TOP/source/flutter_embedder.h $out/src/out/host_$target/flutter_embedder.h
+      done
+    '')
+    {
+      "aarch64-generic" = lib.fakeHash;
+    };
+
+  flutter-engine = if pkgs.flutter-engine.meta.broken
+    then flutter-engines."${targetPlatform.parsed.cpu.name}-generic"
+    else pkgs.flutter-engine;
 in
 stdenv.mkDerivation rec {
   pname = "gokai";
@@ -45,6 +78,10 @@ stdenv.mkDerivation rec {
     repo = "gokai";
     inherit rev;
     sha256 = "sha256-tJXPCCgzAnbRTwPNT9pK0q7GlZErdMYihDIsIliv6mU=";
+  };
+
+  passthru = {
+    inherit flutter-engine;
   };
 
   nativeBuildInputs = [
